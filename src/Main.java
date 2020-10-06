3af4f519824c5677ef4437b23755e05dd71336a6
Nicole Casartelli <44043709+NicCas@users.noreply.github.com>
2020-10-06 00:21:26 -0700

    Update Main.java
    Improvements for testing all required array sizes

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,42 @@
 import java.util.Arrays;
 import java.util.Random;
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-
-        /* Create an int[] array of a predetermined size with random elements */
+    public static void main(String[] args)
+    {
         Random rand = new Random();
-        int arraySize = 1000000;
-        int [] myArray = new int[arraySize];
-        for (int i = 0; i < arraySize; i++) {
-            myArray[i] = rand.nextInt(1000000);
-        }
+        int [] sizes = {100, 1000, 100000, 1000000};
+        int [] myArray = {};
 
-        /* Create Pancake Sort Object */
+        // Create Pancake Sort Object
         PancakeSort stackOJacks = new PancakeSort(myArray);
 
-        /* Print the unsorted array */
-        System.out.println("Unsorted Array: ");
-        System.out.println(Arrays.toString(myArray));
+        /* Create up to 4 unsorted/sorted pairs of arrays of sizes from the array sizes[] */
+        for (int j = 0; j < 4; j++)
+        {
+            // Create new sized array and fill it with random elements
+            myArray = new int[sizes[j]];
+            for (int i = 0; i < sizes[j]; i++) {
+                myArray[i] = rand.nextInt(1000000);
+            }
 
-        /* Print the sorted Array */
-        System.out.println("Sorted Array: ");
-        System.out.println(Arrays.toString(stackOJacks.pancakeSort()));
+            // Declare Pancake Sort Object 
+            stackOJacks = new PancakeSort(myArray);
+
+            // Print the unsorted array
+            System.out.println("Unsorted Array: ");
+            System.out.println(Arrays.toString(myArray));
+
+            // Print the sorted Array
+            System.out.println("Sorted Array: ");
+            System.out.println(Arrays.toString(stackOJacks.pancakeSort()));
+
+            // Print space between pairs of arrays
+            for (int i = 0; i < 4; i++) {
+                System.out.println();
+            }
+        }
 
         /* Used while testing, confirmed that arrays were sorted correctly
 
